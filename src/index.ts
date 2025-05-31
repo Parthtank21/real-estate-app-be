c2a04f1fd6443cc2f430c651e6e5035ad5515e6d
Parth Tank <parthtank301@gmail.com>
2025-05-31 15:13:10 +0530

    feat: complete project setup

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import express from "express";
+import bodyParser from "body-parser";
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get("/home", (req, res) => {
+  res.send("This is home route");
+});
+
+// SERVER
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
